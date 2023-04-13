583fcee7e5155a430d9a92204dd3d8ac01240918
Dayana <dayana.nashkova@yahoo.com>
2023-04-13 22:17:28 +0000

    feat: create function to pick random word from a list

--- run.py
+++ run.py
@@ -1,3 +1,34 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# import random module
+import random
+
+
+def random_word():
+    '''
+    Pick a random word from a list
+    '''
+    words = ['happy', 'computer', 'programming', 'successfully']
+
+    pick_random = random.choice(words)
+    print(pick_random)
+    return pick_random
+
+
+def main():
+    '''
+    Pick a random word from a list
+    '''
+
+    while True:
+        print('Welcome to Jumble Word')
+        play = input('Press P to play or Q to quit\n')
+
+        if play.lower() == 'p':
+            random_word()
+            break
+        elif play.lower() == 'q':
+            quit()
+        else:
+            print('Please valid input. P to play, Q to quit\n')
+
+
+main()
